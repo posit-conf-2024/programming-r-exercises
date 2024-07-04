1da79996ca6a2868efc25573d2de0616dcec71b1
Ian Lyttle <ijlyttle@me.com>
2024-07-04 15:44:30 -0500

    roll your own

--- R/functions-01-advanced-type-check.R
+++ R/functions-01-advanced-type-check.R
@@ -15,6 +15,9 @@ check_string("a")
 check_string(c("hello", "world"))
 check_character(c("hello", "world"))
 
+
+is.numeric(penguins$bill_length_mm)
+
 check_data_frame(mtcars)
 
 # To get this to work in a package:
@@ -28,3 +31,43 @@ check_data_frame(mtcars)
 #
 # At some point, it is imagined that these will be available as functions
 # exported from an "r-lib" package.
+
+# in the standalone files, there is no check_numeric
+# roll your own, adapt `check_logical()`
+check_numeric <- function(x,
+                          ...,
+                          allow_null = FALSE,
+                          arg = caller_arg(x),
+                          call = caller_env()) {
+
+  # predicate
+  if (!missing(x)) {
+    if (is.numeric(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+  }
+
+  # throw error
+  stop_input_type(
+    x,
+    "a numeric vector",
+    ...,
+    allow_na = FALSE,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
+}
+
+to_z <- function(x, middle = 1) {
+  # validate inputs
+  check_numeric(x)
+  check_number_decimal(middle)
+
+  trim = (1 - middle)/2
+  (x - mean(x, na.rm = TRUE, trim = trim)) / sd(x, na.rm = TRUE)
+}
+
